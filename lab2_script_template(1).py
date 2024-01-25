be216823f94d754657fda88724f6f6ac722e83f4
mvirk4 <mvirk@flemingcollege.ca>
2024-01-25 10:31:09 -0500

    step 5 completed

--- lab2_script_template(1).py
+++ lab2_script_template(1).py
@@ -24,9 +24,10 @@ def print_student_name_and_id(about_me):
         f"My name is {full_name}, but you can call me {first_name}./nMy student id is {student_id}."
     )
     return
-    
 # TODO: Step 5 - Function that adds pizza toppings to data structure
 def add_pizza_toppings(about_me, toppings):
+    about_me["pizza_toppings"].append(toppings)
+    print_pizza_toppings(about_me)
     return
 
 # TODO: Step 6 - Function that prints bullet list of pizza toppings
